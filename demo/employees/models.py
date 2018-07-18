58cc5f4eadf8ae1b94765943cb7d3b730a7feb19
Meron Kebede <kebedemeron905@gmail.com>
2018-07-18 14:30:06 -0400

    django app and project setup, employee model created

--- demo/employees/models.py
+++ demo/employees/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Employee(models.Model):
+    firstName = models.TextField()
+    lastName = models.TextField()
+    email = models.TextField()
+    phone = models.TextField()
+    title = models.TextField()
+    department = models.TextField()
+    birthdate = models.TextField() 
